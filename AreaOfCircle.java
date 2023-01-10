8e84c795ecd59b9755ce46340dd656b780231edd
rohit <rohitgiri00@yahoo.com>
2023-01-10 16:06:57 +0530

    area of circle

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -1,5 +1,13 @@
+//Area Of Circle Java Program
+import java.util.Scanner;
+import java.lang.Math;
 public class AreaOfCircle {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter radius of circle");
+        int radius = input.nextInt();
+
+        System.out.printf("area of circle is %f",Math.PI*radius*radius);
     }
 }
