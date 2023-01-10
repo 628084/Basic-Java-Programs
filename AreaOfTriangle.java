7a6997aefec7983c03a7bc3e0913257ee413d066
rohit <rohitgiri00@yahoo.com>
2023-01-10 16:25:46 +0530

    area of circle

---
+++ AreaOfTriangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter height of triangle : ");
+        int height = input.nextInt();
+
+        System.out.print("Enter base of triangle : ");
+        int base = input.nextInt();
+
+        System.out.printf("area of triangle is %f",0.5*base*height);
+
+    }
+}
